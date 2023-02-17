e8b5986843ab7bd38689e25ffb8ad9205a412ca2
dancunrandy <dancunrandy@gmail.com>
2023-02-17 13:24:35 +0300

    commit tree d5dbb8d3dc50e12520dc64ab68fa16c5dcbd8238

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES
+  ('Mr. Chocolate', 5, 'M', 'brown', 'grumpy', true),
+  ('Rowdy', 10, 'M', 'black', 'unpredictable', true),
+  ('Tabitha', 6, 'F', 'white', 'docile', true),
+  ('Sergeant Brown', 12, 'M', 'green', 'militaristic', false),
+  ('Melissa', 4, 'F', 'brown', 'grumpy', true),
+  ('Grinch', 7, 'M', 'green', 'grumpy', true),
+  ('Wendy', 3, 'F', 'pink', 'sweet', true),
+  (NULL, 2, 'F', 'purple', 'unpredictable', true);
